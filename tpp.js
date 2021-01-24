27a3f979be4f82e110ca9388bef8e58d1aa9e6bb
MEOW <2362560018@qq.com>
2021-01-24 14:00:53 +0800

    0

--- tpp.js
+++ tpp.js
@@ -1,7 +1,7 @@
 Game.market.createOrder({
     type: ORDER_BUY,
-    resourceType: RESOURCE_ENERGY,
-    price: 0.214,
-    totalAmount: 300000,
+    resourceType: RESOURCE_POWER,
+    price: 21.4,
+    totalAmount: 23300,
     roomName: "E26N8"
 });
